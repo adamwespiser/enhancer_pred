bfacf7ca45b2196d10aff040cc108b6444506bf2
Adam Wespiser <adam@zlab-wespiser.local>
2014-03-08 01:14:51 -0500

    increasing gbm algorithm performance, expanding gbm comparison groups

--- adam/mlAlgoAW/analysis/runPredOnData.R
+++ adam/mlAlgoAW/analysis/runPredOnData.R
@@ -1,6 +1,9 @@
 
 runMlAlgos <- function(df,cols){
   #   cols <- names(df)[8:22]
+  # don't use -> 
+  # TODO -> figure out exactly why glm has problems resolving subset=train -> eval in wrong envir?
+  return(0)
   train <<- sample(seq_along(df$label), 0.7*dim(df)[1])
   train.X <- as.matrix(df[train,cols])
   test.X <- as.matrix(df[-train,cols])
@@ -95,12 +98,15 @@ runMlAlgosListVersion <- function(df,cols){
   scaleDistro <- TRUE
   
   form <- paste("label ~   ",do.call(paste, c(as.list(cols), sep=" + ")))
+  #hierarchical model -> params fitted seperately for each species
+  form.species <-  paste("label ~  original_species/( ",do.call(paste, c(as.list(cols), sep=" + ")),")") 
   glm.main <- glm(form , data=df[trainVec,], family = binomial)
   glm.main.stats = getStatsFromGlmModel(predict(glm.main,df[-trainVec,], type="response"), as.numeric(df$label[-trainVec]) - 1) 
   
   form.cross <- paste("label ~", do.call(paste, c(as.list(do.call(paste, c(expand.grid(cols, cols), sep=":"))), sep=" + ")))
   
   form.boost <- paste("label_factor ~   ",do.call(paste, c(as.list(cols), sep=" + ")))
+ # form.boost <- paste("label_factor ~   original_species/(", do.call(paste, c(as.list(cols), sep=" + ")),")")
   
   df$label_factor <- factor(df$label)
   
@@ -176,6 +182,33 @@ runMlAlgosListVersion <- function(df,cols){
                         type="response",n.trees=1000),
                         y.test),
                    
+                    
+                    gbm.id1.cv3 = getStatsFromGlmModel(predict(gbm1 <- gbm(formula(form), data = df[trainVec,], distribution="bernoulli", cv.folds=3, 
+                                                             n.trees=10000, interaction.depth=1,verbose=FALSE,shrinkage=0.001), 
+                                                 df[-trainVec,], 
+                                                 type="response",n.trees=gbm.perf(gbm1,method="cv")),
+                                         y.test),
+                    
+                    gbm.id2.cv3 = getStatsFromGlmModel(predict(gbm2 <- gbm(formula(form), data = df[trainVec,], distribution="bernoulli", cv.folds=3, 
+                                                                           n.trees=10000, interaction.depth=2,verbose=FALSE,shrinkage=0.001), 
+                                                               df[-trainVec,], 
+                                                               type="response",n.trees=gbm.perf(gbm2,method="cv")),
+                                                       y.test),
+                    
+                    gbm.id3.cv3 = getStatsFromGlmModel(predict(gbm3 <- gbm(formula(form), data = df[trainVec,], distribution="bernoulli", cv.folds=3, 
+                                                                           n.trees=10000, interaction.depth=3,verbose=FALSE,shrinkage=0.001), 
+                                                               df[-trainVec,], 
+                                                               type="response",n.trees=gbm.perf(gbm3,method="cv")),
+                                                       y.test),
+                    
+                    gbm.id4.cv3 = getStatsFromGlmModel(predict(gbm4 <- gbm(formula(form), data = df[trainVec,], distribution="bernoulli", cv.folds=3, 
+                                                                           n.trees=10000, interaction.depth=4,verbose=FALSE,shrinkage=0.001), 
+                                                               df[-trainVec,], 
+                                                               type="response",n.trees=gbm.perf(gbm4,method="cv")),
+                                                       y.test),
+                    
+                    
+                    
                     C5.0.boost = getStatsFromGlmModel(predict(
                        C5.0(formula(form.boost), data = df[trainVec,],trials=100), 
                             newdata=df[-trainVec,], type="prob",trials=5)[,2],
@@ -222,7 +255,80 @@ accumMlAlgos <- function(df,cols,trials,resultFile,seed=42){
   collect.df
 }
 
+# optimize gbm
 
+getBestGBMparams <- function(df,cols, outdir){
+  trainVec <- sample(seq_along(df$label), 0.7*dim(df)[1])
+  trainVecForGlm <- trainVec
+  trainVec.X <- as.matrix(df[trainVec,cols])
+  test.X <- as.matrix(df[-trainVec,cols])
+  y.trainVec <- df$label[trainVec]
+  y.test <- df$label[-trainVec]
+  y.freq0 <- length(which(y.trainVec == 0))/length(y.trainVec)
+  y.freq1 <- length(which(y.trainVec == 1))/length(y.trainVec)
+  weight.trainVec <<- ifelse(y.trainVec== 1, 1/y.freq1, 1/y.freq0) 
+  grid = 10 ^ seq(10, -2, length = 100)
+  scaleDistro <- TRUE  
+  form <- paste("label ~   ",do.call(paste, c(as.list(cols), sep=" + ")))
+  form.species <-  paste("label ~  original_species/( ",do.call(paste, c(as.list(cols), sep=" + ")),")")   
+  form.cross <- paste("label ~", do.call(paste, c(as.list(do.call(paste, c(expand.grid(cols, cols), sep=":"))), sep=" + ")))
+  form.boost <- paste("label_factor ~   ",do.call(paste, c(as.list(cols), sep=" + ")))
+  # form.boost <- paste("label_factor ~   original_species/(", do.call(paste, c(as.list(cols), sep=" + ")),")")
+  df$label_factor <- factor(df$label)
+  
+  
+  gbm1 <-  gbm(formula(form), data = df[trainVec,], distribution="bernoulli", cv.folds=3, 
+                   n.trees=10000, interaction.depth=1,verbose=FALSE)
+  
+  model1.stats <- getStatsFromGlmModel(predict(gbm1, 
+    df[-trainVec,], 
+    type="response",n.trees=gbm.perf(gbm1,method="cv")),
+    y.test)
+  
+  gbm1.sum <- summary(gbm1)
+  gbm1.colRank <- as.numeric(unlist(sapply(cols, function(x)which( as.character((gbm1.sum)$var) == x))))
+  
+  cumsum(rev(summary(gbm1)$rel.inf))/sum(summary(gbm1)$rel.inf)
+  
+  ggplot(summary(gbm1), aes(x=var,y=rel.inf))+geom_histogram(stat="identity")+coord_flip()
+  
+  cum.rel.inf <- cumsum(rev(summary(gbm1)$rel.inf))/sum(summary(gbm1)$rel.inf)
+  cols.model2 <- cols[-which(cum.rel.inf < 0.02)]
+  form.model2 <- paste("label ~   ",do.call(paste, c(as.list(cols.model2), sep=" + ")))
+  
+  gbm2 <-  gbm(formula(form.model2), data = df[trainVec,], distribution="bernoulli",
+                    cv.folds=3, 
+                   n.trees=10000,
+                   interaction.depth=2,verbose=FALSE)
+  
+  model2.stats <- getStatsFromGlmModel(predict(gbm2, 
+                                               df[-trainVec,], 
+                                               type="response",n.trees=gbm.perf(gbm2,method="cv")),
+                                       y.test)
+  
+  
+  ## OOB estimate is warned against -> going with cv to train on the number of trees
+  
+  gbm3 <-  gbm(formula(form), data = df[trainVec,], distribution="adaboost", cv.folds=3, 
+               n.trees=10000, interaction.depth=2,verbose=FALSE)
+  
+  model3.stats <- getStatsFromGlmModel(predict(gbm3, 
+                                               df[-trainVec,], 
+                                               type="response",n.trees=gbm.perf(gbm3,method="cv")),
+                                       y.test)
+  
+  
+  
+  model1.stats <- getStatsFromGlmModel(predict(gbm1 <- gbm(formula(form), data = df[trainVec,], distribution="bernoulli", cv.folds=3, 
+                                                   n.trees=4000, interaction.depth=1,verbose=FALSE,shrinkage=0.01), 
+                                               df[-trainVec,], 
+                                               type="response",n.trees=gbm.perf(gbm1,method="cv")),
+                                       y.test)
+  
+  
+  
+  
+}
 
 
 # eval errors w/ test case
