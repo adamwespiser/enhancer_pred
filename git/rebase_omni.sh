c9bdfbbe8e63ec33c56583c2ad54c9dc627f95a8
Adam Wespiser <adam@zlab-wespiser.local>
2014-03-10 17:32:07 -0400

    removing michael's git repo

--- git/rebase_omni.sh
+++
@@ -1,1 +0,0 @@
-g rebase -i -p -srecursive -Xpatience -Xignore-all-space d5dd48e0cde0f5a6c952931b18ad7e648dadcdbb
--- git/repack.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import sys, os
-
-sys.path.append(os.path.expanduser('~/Git.bitbucket/common/python'))
-from git import Git
-
-Git.repack()
